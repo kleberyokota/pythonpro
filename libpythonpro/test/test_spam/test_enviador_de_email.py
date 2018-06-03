f58eb98b89f69aafb6d52a7eaa40851496b0ce92
kleberyokota <kleberyokota@unknown.home>
2018-06-03 17:50:15 -0300

    Ajuste com flake8

--- libpythonpro/test/test_spam/test_enviador_de_email.py
+++ libpythonpro/test/test_spam/test_enviador_de_email.py
@@ -7,6 +7,7 @@ def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
 
+
 @pytest.mark.parametrize('destinatario', ['kleber_yokota@hotmail.com', 'foo@bar.com.br'])
 def teste_remetente(destinatario):
     enviador = Enviador()
