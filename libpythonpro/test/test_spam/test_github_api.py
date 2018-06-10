3a94721abf8e0eb99ce077af2bab866fa0992e9c
kleberyokota <kleber_yokota@hotmail.com>
2018-06-10 16:12:07 -0300

    troca da URL no teste de busca de avatar

--- libpythonpro/test/test_spam/test_github_api.py
+++ libpythonpro/test/test_spam/test_github_api.py
@@ -8,7 +8,7 @@ from libpythonpro import github_api
 @pytest.fixture
 def avatar_url(mocker):
     resp_mock = Mock()
-    url = 'https://avatars2.githubusercontent.com/u/109299?v=4'
+    url = 'https://avatars2.githubusercontent.com/u/39857180?v=4'
     resp_mock.json.return_value = {
         'login': 'kleberyokota', 'id': 39857180,
         'avatar_url': url
@@ -25,5 +25,5 @@ def test_buscar_avatar(avatar_url):
 
 
 def test_buscar_avatar_integracao():
-    url = github_api.buscar_avatar('kleber')
-    assert 'https://avatars2.githubusercontent.com/u/109299?v=4' == url
+    url = github_api.buscar_avatar('kleberyokota')
+    assert 'https://avatars2.githubusercontent.com/u/39857180?v=4' == url
